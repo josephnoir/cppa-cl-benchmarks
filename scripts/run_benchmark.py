5591ee1310911e7e5a8f3dc07570eabebc2911ab
Joseph Noir <noir@mobi19.cpt.haw-hamburg.de>
2013-12-20 16:00:25 +0100

    added options to allow a range of problem sizes
    -s is the lower boundry
    -e is the upper boundry
    -a is the increment for each step
    -t is a testing mode wich does not write output to files

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -14,14 +14,37 @@ result_path = root_path + "results/"
 
 bins = [f for f in listdir(bin_path) if isfile(join(bin_path, f))]
 
+def benchmark(executable, iterations, size, postfix, samples, test):
+    print("Running 'time {0} -i {1} -s {2}' for {3} samples".format(executable, iterations, size, samples))
+    file_name = result_path + executable + postfix + ".dat"
+    if not test:
+        fd = open(file_name, "w")
+    print("Output to: {}".format(file_name))
+    for n in range(0, samples):
+        rc = subprocess.check_output(["time", bin_path + executable, "-i", str(iterations), "-s", str(size)], 
+                                     universal_newlines=True, stderr=subprocess.STDOUT)
+        rc = [i for i in rc.split(' ') if i != ''][0]
+        if not test:
+            fd.write("%s\n" % rc)
+        print("[{0}] {1}".format(n, rc))
+    if not test:
+        fd.close()
+    print("Done.")
+
+
 def main():
     parser = OptionParser(version="%prog 0.1")
-    parser.add_option("-i", "--iterations", action="store", type="int", dest="iter",
-                      help="set number of iterations [default=7]", default=7)
-    parser.add_option("-s", "--size"      , action="store", type="int", dest="size",
-                      help="set problem size [default=1]", default=1)
+    parser.add_option("-i", "--iterations"     , action="store", type="int", dest="iterations",
+                      help="set number of iterations [default=7]"                 , default=7)
+    parser.add_option("-s", "--size"           , action="store", type="int", dest="size",
+                      help="set problem size [default=1]"                         , default=1)
+    parser.add_option("-e", "--end"            , action="store", type="int", dest="end",
+                      help="make benchmarks for different sizes [size end]"       , default=0)
+    parser.add_option("-a", "--increment"      , action="store", type="int", dest="increment",
+                      help="increment size after each sample until end is reached", default=0)
     parser.add_option("-n", "--numberofsamples", action="store", type="int", dest="samples",
-                      help="number of samples per program", default=1)
+                      help="number of samples per program"                        , default=1)
+    parser.add_option("-t", "--test"           , action="store_true", dest="test" , default=False)
 
     (options, args) = parser.parse_args()
     if len(args) < 1:
@@ -30,22 +53,26 @@ def main():
     if len(unknown) != 0:
         parser.error("[!!!] Unknown executable: " + repr(unknown))
 
-    for e in args:
-        print("Running 'time {0} -i {1} -s {2}'".format(e, options.iter, options.size))
-        if not os.path.exists(result_path):
-            os.makedirs(result_path)
-        fd = open(result_path + e + ".dat", "w")
-        for n in range(0, options.samples):
-            rc = subprocess.check_output(["time", bin_path + e, "-i", str(options.iter), "-s", str(options.size)], 
-                                         universal_newlines=True,
-                                         stderr=subprocess.STDOUT)
-            rc = [i for i in rc.split(' ') if i != ''][0]
-            fd.write("%s\n" % rc)
-            print("[{0}] Output: {1}".format(n, rc))
-        fd.close()
-        print("Done.")
+    # make sure directory for results exists
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
+    if options.end <= options.size:
+        postfix = ""
+        for e in args:
+            benchmark(e, options.iterations, options.size, postfix, options.samples, options.test)
+    elif options.end > options.size and options.increment > 0:
+        sample_sizes = range(options.size, options.end + 1, options.increment)
+        for e in args:
+            for s in sample_sizes:
+                postfix = "_" + repr(s)
+                benchmark(e, options.iterations, s, postfix, options.samples, options.test)
+    else:
+        parser.error("[!!!] Increment needs to be greater than 0.")
 
 
 if __name__ == "__main__":
     main()
+
+
 
